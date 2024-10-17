e480179cc7fb9867bfd11ffbec0dba550621ecf3
Suene Souza <suene.vs@gmail.com>
2024-10-16 22:56:53 -0300

    Corrigido lógica condicional isLoginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,6 @@ export class AppComponent {
   constructor(private router: Router) {}
 
   isLoginPage(): boolean {
-    return this.router.url === '/login';
+    return this.router.url === '/login' || this.router.url ==='/unauthenticated';
   }
 }
