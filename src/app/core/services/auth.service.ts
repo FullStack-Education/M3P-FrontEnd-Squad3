14224413bd5fbb9b582464a60733ceba222937f9
Leandro Dias <lvd@certi.org.br>
2024-10-28 23:38:03 -0300

    limpado service de auth

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,78 +2,58 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
-import { IUser } from '../interfaces/user.interface';
-import { IRole } from '../interfaces/role.interface';
-import { environment } from '../../../environments/environment';
+import AuthTokenService from './auth-token.service';
+import { IResponse } from '../interfaces/response.inteface';
+import { IToken } from '../interfaces/Itoken.inteface';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUsersUrl = environment.apiUrl +'/usuario';
-  private apiRolesUrl = environment.apiUrl +'/papel';
-  private currentUserKey = 'currentUser';
+  private apiLoginUrl = '/api/login';
 
-  constructor(private http: HttpClient) {}
+  private apiUsersUrl = 'http://localhost:3000/usuario';
+  private apiRolesUrl = 'http://localhost:3000/papel';
+  private currentUserKey = 'token';
 
-  signIn(email: string, password: string): Observable<IUser | null> {    
-    const userWithRole = this.findUser(email, password).pipe(
-      switchMap((user) => (user ? this.fetchUserRole(user) : of(null))),
-      catchError((error) => {
-        console.error('Error during sign-in process:', error);
-        return of(null);
-      })
+  constructor(private http: HttpClient, private authToken :AuthTokenService) { }
+
+  signIn(email: string, password: string): Observable<IToken | null> {    
+    return  this.findLogin(email, password).pipe(
+        map((user: IResponse) => {
+            localStorage.setItem(this.currentUserKey,user.authToken)
+            return this.authToken.decodePayloadJWT();
+        }),
+        map((user: IToken) => user),
+        catchError(() => of(null))
     );
-    if (userWithRole) {
-      userWithRole.subscribe((user) => {
-        localStorage.setItem(this.currentUserKey, JSON.stringify(user));
-      });
-    }
-    return userWithRole;
+    
   }
 
-  private findUser(
+  private findLogin(
     email: string,
     password: string
-  ): Observable<IUser | null> {
-    console.log(this.apiUsersUrl)
-    return this.http.get<IUser[]>(this.apiUsersUrl).pipe(
-      map(
-        (users) => {
-          return users.find(
-            (user) => user.email === email && user.password === password
-          ) || null
-        }
-      ),
-      catchError(() => of(null))
-    );
+  ): Observable<IResponse> {
+    return this.http.post<IResponse>(this.apiLoginUrl,{
+      'login':email,
+      'senha':password
+    });
   }
 
-  private fetchUserRole(user: IUser): Observable<IUser> {
-    return this.http.get<IRole[]>(this.apiRolesUrl).pipe(
-      map((roles) => roles.find((role) => {
-        console.log(role.id, user.papelId, role.id === user.papelId);
-        
-        return role.id === user.papelId
-      })),
-      map((role) => ({
-        ...user,
-        role: role || { id: '0', name: 'Unknown' },
-      }))
-    );
-  }
+ 
 
 
   logOut(): void {
     localStorage.removeItem(this.currentUserKey);
   }
 
-  getCurrentUser(): IUser {
+  getCurrentUser(): IToken {
     const userJson = localStorage.getItem(this.currentUserKey);
     if (!userJson) {
       throw new Error('Error retrieving the current user details: no user found in local storage');
     }
-    return userJson ? JSON.parse(userJson) : null;
+    return this.authToken.decodePayloadJWT();
   }
 
   isAuthenticated(): boolean {
@@ -82,11 +62,11 @@ export class AuthService {
 
   isAdmin() {
     const user = this.getCurrentUser();
-    return user?.role?.name === 'Admin';
+    return user?.scope === 'ADM';
   }
 
   isTeacher() {
     const user = this.getCurrentUser();
-    return user?.role?.name === 'Docente';
+    return user?.scope == "PROFESSOR";
   }
 }
